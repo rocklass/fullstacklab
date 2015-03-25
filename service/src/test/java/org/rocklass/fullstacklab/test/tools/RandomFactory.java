a9dfc14d1a7cfcc0fc396009d112e0ea43da19d6
rocklass <patrick.brochado@gmail.com>
2015-03-25 23:26:07 +0100

    PMD config file

--- service/src/test/java/org/rocklass/fullstacklab/test/tools/RandomFactory.java
+++ service/src/test/java/org/rocklass/fullstacklab/test/tools/RandomFactory.java
@@ -7,7 +7,7 @@ import org.apache.commons.lang3.RandomUtils;
 import org.rocklass.fullstacklab.model.Item;
 
 public final class RandomFactory {
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
     
     private RandomFactory() {
     }
@@ -15,7 +15,7 @@ public final class RandomFactory {
     public static Item createItem() {
         final Item item = new Item();
         item.setId(RandomUtils.nextLong(0, Long.MAX_VALUE));
-        item.setChecked(random.nextBoolean());
+        item.setChecked(RANDOM.nextBoolean());
         item.setDescription(RandomStringUtils.random(RandomUtils.nextInt(0, 63)));
         return item;
     }
